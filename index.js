4a36717bc45f3018c692477042f29b8a6a3ec0f9
Luciano Bellofatto <58310887+luchob89@users.noreply.github.com>
2024-12-13 14:13:01 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ mongoose.connect(process.env.MONGO_URI);
 
 const userSchema = new mongoose.Schema({
   username: String,
-  description: String,
-  duration: Number,
-  date: String
+  exercises: [{
+    description: String,
+    duration: Number,
+    date: String
+  }]
 });
 
 const User = mongoose.model('User', userSchema);
@@ -66,6 +68,15 @@ app.get('/api/users', async (req, res) => {
 // Users POST create new exercise for 1 user
 app.post('/api/users/:_id/exercises', async (req, res) => {
 
+  // ELLOS MANDAN 11 INTENTOS
+  // YO CREO 10 EJERCICIOS
+  //console.log('INTENTO DE CREACION DE NUEVO EJERCICIO')
+
+  // console.log('POST exercise')
+  // console.log('req.body: ', req.body)
+  // console.log('req.params: ', req.params)
+  // console.log('req.query: ', req.query)
+
   let date = new Date().toDateString();
 
   // Had to add 1 day extra because of timezone difference
@@ -75,89 +86,100 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
     date = new Date(plusOneDay).toDateString();
   }
 
-  const updateUser = await User.updateOne({ _id: req.params._id }, {
-    description: req.body.description,
-    duration: req.body.duration,
-    date: date
+  const newSolution = await User.findByIdAndUpdate(req.params._id, {
+    $push: { 
+      exercises: {
+        description: req.body.description,
+        duration: req.body.duration,
+        date: date
+      }
+    }
   })
+
+  console.log('New solution: ', newSolution)
+
+  // const updateUser = await User.updateOne({ _id: req.params._id }, {
+  //   description: req.body.description,
+  //   duration: req.body.duration,
+  //   date: date
+  // })
 
   //console.log('New exercise result: ', updateUser)
 
   // Db query
-  const queryResult = await User.findById(req.params._id, 'username description duration date _id').exec();
+  //const queryResult = await User.findById(req.params._id, 'username description duration date _id').exec();
 
-  //console.log('', queryResult)
+  //console.log('Response: ', queryResult)
 
-  return res.json(queryResult)
+  return res.json({
+    username: newSolution.username,
+    description: req.body.description,
+    duration: req.body.duration,
+    date: date,
+    _id: req.params._id
+  })
 
 })
 
 // GET all exercises from specific user
 app.get('/api/users/:_id/logs', async (req, res) => {
 
-  //let query;
-  // Optional query parameters
-  // if ( req.query.from || req.query.to || req.query.limit ) {
-  //   // Adding 'from' and 'to' to date parameter
-  //   query = User.find({
-  //     _id: req.params._id,
-  //     date: { 
-  //       $gt :  new Date(req.query.from).toDateString(), 
-  //       $lt : new Date(req.query.to).toDateString()
-  //     }
-  //   })
-  //   // Adding limit
-  //   query.limit(req.query.limit)
-  // }
-  // else query = User.find({_id: req.params._id})
-
-
   // Db query
-  const queryResult = await User.find({_id: req.params._id}).exec();
+  const userExercises = await User.findById(req.params._id).exec();
 
-  console.log('GET All logs from User: ', queryResult)
+  console.log('userExercises: ', userExercises)
 
-  // Initial response object
-  let responseObject = {
-    _id: req.params._id,
-    log: []
-  }
+  const { to, from, limit} = req.query;
 
-  // Loop over results to fill response object properly
-  for ( let i = 0; i < queryResult.length; i++ ) {
-
-    responseObject.username = queryResult[i].username
-
-    const { to, from, limit} = req.query;
-
-    if ( from || to || limit ) {
-
-      console.log('from: ', from)
-      console.log('to: ', to)
-      console.log('limit: ', limit)
-
-      console.log(req)
-
-      if ( queryResult[i].date < new Date(from).toDateString() ) continue;
-      if ( queryResult[i].date > new Date(to).toDateString() ) continue;
-      if ( i > limit ) break;
-    }
-
-    responseObject.log.push({
-      description: queryResult[i].description,
-      duration: queryResult[i].duration,
-      date: queryResult[i].date,
-    })
-  }
-
-  // /api/users/675889ccbcc861589f84fd58/logs?from=1928-05-05&to=2050-05-05&limit=5
-
-  responseObject.count = responseObject.log.length;
-
-  console.log('Response: ', responseObject)
+  if ( from ) userExercises.exercises.filter( exercise => exercise.date > new Date(from).toDateString() )
+  if ( to ) userExercises.exercises.filter( exercise => exercise.date < new Date(to).toDateString() )
+  if ( limit ) userExercises.exercises.slice(0, limit)
 
   // Response
-  return res.json(responseObject)
+  return res.json({
+    username: userExercises.username,
+    count: userExercises.exercises.length,
+    _id: req.params._id,
+    log: userExercises.exercises
+  })
+
+  // // Loop over results to fill response object properly
+  // for ( let i = 0; i < queryResult.length; i++ ) {
+
+  //   responseObject.username = queryResult[i].username
+
+  //   const { to, from, limit} = req.query;
+
+  //   if ( from || to || limit ) {
+
+  //     //console.log('from: ', from)
+  //     //console.log('to: ', to)
+  //     //console.log('limit: ', limit)
+
+  //     //console.log(req)
+
+  //     if ( queryResult[i].date < new Date(from).toDateString() ) continue; // from=1989-12-31
+  //     if ( queryResult[i].date > new Date(to).toDateString() ) continue; // to=1990-01-04
+  //     if ( limit !== undefined && i > limit ) break;
+  //   }
+
+  //   responseObject.log.push({
+  //     description: queryResult[i].description,
+  //     duration: queryResult[i].duration,
+  //     date: queryResult[i].date,
+  //   })
+  // }
+
+  // // /api/users/675889ccbcc861589f84fd58/logs?from=1928-05-05&to=2050-05-05&limit=5
+  // // ESTO MANDA FCC: SIN LIMIT
+  // // /api/users/6759e326ac394723fa4164d9/logs?from=1989-12-31&to=1990-01-04
+
+  // responseObject.count = responseObject.log.length;
+
+  // console.log('Response: ', responseObject)
+
+  // // Response
+  // return res.json(responseObject)
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
